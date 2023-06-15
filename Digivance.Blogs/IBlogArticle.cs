c9b1a3ba8869661193c3ae671429f2f0b5d50c2e
DigiGunjayas <136254626+DigiGunjayas@users.noreply.github.com>
2023-06-15 21:27:47 +0545

    Added blog article and IBlogarticle (#6)

---
+++ Digivance.Blogs/IBlogArticle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs
+{
+    /// <summary>
+    /// Represents a blog article
+    /// </summary>
+    public interface IBlogArticle
+    {
+
+        /// <summary>
+        /// The ID of the author associated with the blog article.
+        /// </summary>
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// The Text content of the blog article.
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// The subtitle of the blog article.
+        /// </summary>
+        public string? SubTitle { get; set; }
+
+        /// <summary>
+        /// The title of the blog article.
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
