ea9f787076f2274be74676c46283ad8353b47542
DigiGunjayas <gshrestha@digivance.com>
2023-06-15 22:01:35 +0545

    Added CreateBlog and UpdateBlog commands

---
+++ Digivance.Blogs/Commands/UpdateBlogArticle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs.Commands
+{
+    /// <summary>
+    /// A command object containing all of the fields necessary to update a Blog Article
+    /// </summary>
+    public class UpdateBlogArticle : IBlogArticle
+    {
+        /// <inheritdoc />
+        public int AuthorId { get; set; }
+
+        /// <inheritdoc />
+        public string Content { get; set; }
+
+        /// <inheritdoc />
+        public string Id { get; set; }
+
+        /// <inheritdoc />
+        public string? SubTitle { get; set; }
+
+        /// <inheritdoc />
+        public string Title { get; set; }
+    }
+}
