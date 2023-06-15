2e2e1463f0ae2592535b1b98a63c906af2e70966
DigiGunjayas <136254626+DigiGunjayas@users.noreply.github.com>
2023-06-15 22:22:12 +0545

    Added CreateBlog and UpdateBlog commands (#7)
    * Added CreateBlog and UpdateBlog commands
    
    * Changed comment for UpdateBlog's Id

---
+++ Digivance.Blogs/Commands/CreateBlogArticle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs.Commands
+{
+    /// <summary>
+    /// A command object containing all of the necessary fields to create a new
+    /// Blog Article.
+    /// </summary>
+    public class CreateBlogArticle : IBlogArticle
+    {
+        /// <inheritdoc />
+        public int AuthorId { get; set; }
+
+        /// <inheritdoc />
+        public string Content { get; set; }
+
+        /// <inheritdoc />
+        public string? SubTitle { get; set; }
+
+        /// <inheritdoc />
+        public string Title { get; set; }
+    }
+
+}
+
