f2becd9ac6dd11206c55e057e21d868f643116e1
DigiGunjayas <gshrestha@digivance.com>
2023-06-15 20:53:09 +0545

    Added blog article and IBlogarticle

---
+++ Digivance.Blogs/BlogArticle.cs
@@ -0,0 +1,42 @@
+﻿using Digivance.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs
+{
+    /// <summary>
+    /// Represents a blog article
+    /// </summary>
+    public class BlogArticle : IBlogArticle, IAuditable
+    {
+        /// <inheritdoc />
+        public int AuthorId { get; set; }
+
+        /// <inheritdoc />
+        public string Content { get; set; }
+
+        /// <inheritdoc />
+        public int CreatedById { get; set; }
+
+        /// <inheritdoc />
+        public DateTime CreatedDate { get; set; }
+
+        /// <inheritdoc />
+        public int Id { get; set; }
+
+        /// <inheritdoc />
+        public int? ModifiedById { get; set; }
+
+        /// <inheritdoc />
+        public DateTime? ModifiedDate { get; set; }
+
+        /// <inheritdoc />
+        public string? SubTitle { get; set; }
+
+        /// <inheritdoc />
+        public string Title { get; set; }
+    }
+}
