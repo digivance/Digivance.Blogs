9b2445cd51726b845983b3c6692a4d86618b6522
DigiGunjayas <gshrestha@digivance.com>
2023-06-16 08:19:51 +0545

    InMemory and Tests

---
+++ Digivance.Blogs/Services/IBlogArticleService.cs
@@ -0,0 +1,47 @@
+﻿using Digivance.Blogs.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs.Services
+{
+    /// <summary>
+    /// Used to indicate a service object capable of handling Blog Article functionality.
+    /// </summary>
+    public interface IBlogArticleService
+    {
+        /// <summary>
+        /// Create and persist a new blog article record, returns a model representing the newly created blog article.
+        /// </summary>
+        /// <param name="command">The CreateBlogArticle command to execute</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>BlogArticle model representing the newly created blog article</returns>
+        public Task<BlogArticle> CreateBlogArticleAsync(CreateBlogArticle command, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes a blog article from storage.
+        /// </summary>
+        /// <param name="id">Unique ID of the blog article to remove</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>Task to await</returns>
+        public Task DeleteBlogArticleAsync(int id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Returns a blog article by its unique ID.
+        /// </summary>
+        /// <param name="id">Unique ID of the blog article to return</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>BlogArticle model representing the requested blog article</returns>
+        public Task<BlogArticle> GetBlogArticleAsync(int id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Updates and persists a blog article and returns a BlogArticle model representing the updated blog article.
+        /// </summary>
+        /// <param name="command">The UpdateBlogArticle command to execute</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>BlogArticle model representing the updated blog article</returns>
+        public Task<BlogArticle> UpdateBlogArticleAsync(UpdateBlogArticle command, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Digivance.Blogs/Services/InMemoryBlogArticleService.cs
@@ -0,0 +1,73 @@
+﻿using Digivance.Blogs.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Blogs.Services
+{
+    /// <summary>
+    /// Very basic implementation of BlogArticleService in memory, note that the BlogArticles
+    /// collection is static to support scoped dependency injection.
+    /// </summary>
+    public class InMemoryBlogArticleService : IBlogArticleService
+    {
+        /// <summary>
+        /// The "database" of blog articles
+        /// </summary>
+        protected static List<BlogArticle> BlogArticles = new List<BlogArticle>();
+
+        /// <inheritdoc />
+        public Task<BlogArticle> CreateBlogArticleAsync(CreateBlogArticle command, CancellationToken cancellationToken = default)
+        {
+            var newBlogArticle = new BlogArticle
+            {
+                AuthorId = command.AuthorId,
+                Content = command.Content,
+                CreatedById = 0,
+                CreatedDate = DateTime.UtcNow,
+                SubTitle = command.SubTitle,
+                Title = command.Title,
+            };
+
+            BlogArticles.Add(newBlogArticle);
+            return Task.FromResult(newBlogArticle);
+        }
+
+        /// <inheritdoc />
+        public Task DeleteBlogArticleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var existingBlogArticle = BlogArticles.FirstOrDefault(article => article.Id == id);
+            if (existingBlogArticle != null)
+                BlogArticles.Remove(existingBlogArticle);
+
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task<BlogArticle> GetBlogArticleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var existingBlogArticle = BlogArticles.FirstOrDefault(article => article.Id == id);
+            if (existingBlogArticle == null)
+                throw new Exception("Blog article not found");
+
+            return Task.FromResult(existingBlogArticle);
+        }
+
+        /// <inheritdoc />
+        public Task<BlogArticle> UpdateBlogArticleAsync(UpdateBlogArticle command, CancellationToken cancellationToken = default)
+        {
+            var existingBlogArticle = BlogArticles.FirstOrDefault(article => article.Id == command.Id);
+            if (existingBlogArticle == null)
+                throw new Exception("Blog article not found");
+
+            existingBlogArticle.Content = command.Content;
+            existingBlogArticle.ModifiedDate = DateTime.UtcNow;
+            existingBlogArticle.SubTitle = command.SubTitle;
+            existingBlogArticle.Title = command.Title;
+
+            return Task.FromResult(existingBlogArticle);
+        }
+    }
+}
